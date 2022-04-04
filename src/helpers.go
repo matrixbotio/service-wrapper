eed79f08a21324762d85eec32524879982abe51a
Nikita Zhdanov <wsleepwalker@gmail.com>
2022-04-04 16:22:23 +0200

    Revert "fix healthcheck when stopped by telegram bot"
    This reverts commit bab0c2a46a0a154354bb833f4595f95c394dce94.

--- src/helpers.go
+++ src/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"os"
 	"strings"
@@ -13,16 +14,17 @@ import (
 )
 
 var timeFormat = "2006-01-02 15:04:05"
+var stoppedFile = "/wrapper-process-stopped"
 
 func appendToLimitedArr(arr []string, str string, count int) []string {
-	if len(arr) > count-1 {
-		arr = arr[len(arr)-count+1:]
+	if len(arr) > count - 1 {
+		arr = arr[len(arr) - count + 1:]
 	}
 	return append(arr, str)
 }
 
 func readData(readCloser io.ReadCloser, onNewLine func(string)) *promise.Promise {
-	return promise.New(func(resolve func(v promise.Any), reject func(error)) {
+	return promise.New(func(resolve func(v promise.Any), reject func(error)){
 		lastline := ""
 		buf := make([]byte, 8)
 		for {
@@ -30,11 +32,11 @@ func readData(readCloser io.ReadCloser, onNewLine func(string)) *promise.Promise
 			lastline = lastline + string(buf[:n])
 			if strings.Contains(lastline, "\n") {
 				splitted := strings.Split(lastline, "\n")
-				linesToAdd := splitted[:len(splitted)-1]
+				linesToAdd := splitted[:len(splitted) - 1]
 				for i := 0; i < len(linesToAdd); i++ {
 					onNewLine(linesToAdd[i])
 				}
-				lastline = splitted[len(splitted)-1]
+				lastline = splitted[len(splitted) - 1]
 			}
 			if err == io.EOF {
 				if lastline != "" {
@@ -54,13 +56,13 @@ func filesStdoutStderr(stdoutbuf []string, stderrbuf []string) []controller.File
 	stderr := strings.Join(stderrbuf, "\n")
 	if len(stdout) > 0 {
 		files = append(files, controller.File{
-			Name:    processDef.ServiceName + " — " + timestr + ".stdout.txt",
+			Name: processDef.ServiceName + " — " + timestr + ".stdout.txt",
 			Content: strings.Join(stdoutbuf, "\n"),
 		})
 	}
 	if len(stderr) > 0 {
 		files = append(files, controller.File{
-			Name:    processDef.ServiceName + " — " + timestr + ".stderr.txt",
+			Name: processDef.ServiceName + " — " + timestr + ".stderr.txt",
 			Content: strings.Join(stderrbuf, "\n"),
 		})
 	}
@@ -72,33 +74,34 @@ func filesLinebuf(linebuf []string) []controller.File {
 	output := strings.Join(linebuf, "\n")
 	if len(output) > 0 {
 		files = append(files, controller.File{
-			Name:    processDef.ServiceName + " — " + time.Now().Format(timeFormat) + ".output.txt",
+			Name: processDef.ServiceName + " — " + time.Now().Format(timeFormat) + ".output.txt",
 			Content: strings.Join(linebuf, "\n"),
 		})
 	}
 	return files
 }
 
-func sendSig(sig os.Signal) {
+func sendSig(sig os.Signal){
 	if cmd == nil {
 		return
 	}
 	cmd.Process.Signal(sig)
 }
 
-func notifyExternalProcessesStopped() {
-	stopped = true
+func notifyExternalProcessesStopped(){
+	os.OpenFile(stoppedFile, os.O_RDONLY | os.O_CREATE, 0666)
 }
 
-func notifyExternalProcessesStarted() {
-	stopped = false
+func notifyExternalProcessesStarted(){
+	os.Remove(stoppedFile)
 }
 
 func isProcessStoppedByWrapper() bool {
-	return stopped
+	_, err := os.Stat(stoppedFile)
+	return errors.Is(err, os.ErrNotExist)
 }
 
-func stopProcess() {
+func stopProcess(){
 	if !running {
 		return
 	}
@@ -108,23 +111,22 @@ func stopProcess() {
 	sendSig(syscall.SIGSTOP)
 }
 
-func resumeProcess() {
+func resumeProcess(){
 	if !paused {
 		return
 	}
 	paused = false
 	running = true
-	notifyExternalProcessesStarted()
 	sendSig(syscall.SIGCONT)
 }
 
-func termProcess() {
+func termProcess(){
 	resumeProcess()
 	notifyExternalProcessesStopped()
 	sendSig(syscall.SIGTERM)
 }
 
-func killProcess() {
+func killProcess(){
 	resumeProcess()
 	notifyExternalProcessesStopped()
 	sendSig(syscall.SIGKILL)
@@ -134,31 +136,31 @@ func statusButtons() []controller.Button {
 	if running {
 		return []controller.Button{{
 			RMMsgsOnClick: true,
-			Text:          "⏸ Пауза",
-			OnClick:       stopProcess,
+			Text: "⏸ Пауза",
+			OnClick: stopProcess,
 		}, {
 			RMMsgsOnClick: true,
-			Text:          "⏹ Остановить",
-			OnClick:       termProcess,
+			Text: "⏹ Остановить",
+			OnClick: termProcess,
 		}, {
 			RMMsgsOnClick: true,
-			Text:          "⛔️ Убить",
-			OnClick:       killProcess,
+			Text: "⛔️ Убить",
+			OnClick: killProcess,
 		}}
 	}
 	if paused {
 		return []controller.Button{{
 			RMMsgsOnClick: true,
-			Text:          "▶️ Продолжить",
-			OnClick:       resumeProcess,
+			Text: "▶️ Продолжить",
+			OnClick: resumeProcess,
 		}, {
 			RMMsgsOnClick: true,
-			Text:          "⏹ Остановить",
-			OnClick:       termProcess,
+			Text: "⏹ Остановить",
+			OnClick: termProcess,
 		}, {
 			RMMsgsOnClick: true,
-			Text:          "⛔️ Убить",
-			OnClick:       killProcess,
+			Text: "⛔️ Убить",
+			OnClick: killProcess,
 		}}
 	}
 	return []controller.Button{}
